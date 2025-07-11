a1f69a360f222ebbe5e0b14839a99e0ff0292284
Farouta <fourat.amri@hotmail.com>
2025-07-11 14:37:29 +0200

    update

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,4 +1,4 @@
-let idx, data;
+var idx, data;
 
 fetch('/search_data.json')
   .then(res => res.json())
@@ -18,34 +18,42 @@ fetch('/search_data.json')
     });
   });
 
-const input = document.getElementById('search_box');
-const resultsContainer = document.getElementById('search_results');
+const navbar_input = document.getElementById('navbar_search_box');
+const navbar_resultsContainer = document.getElementById('navbar_search_results');
 
-input.addEventListener('input', () => {
-  const query = input.value.trim();
-  resultsContainer.innerHTML = '';
+navbar_input.addEventListener('input', () => {
+  const query = navbar_input.value.trim();
+  navbar_resultsContainer.innerHTML = '';
 
   const results = idx.search(`${query}*`);
-
-  if (results.length) {
-    results.slice(0, 5).forEach(r => {
+  if (results.length>0) {
+    results.slice(0,5).forEach(r => {
       const match = data.find(d => d.url === r.ref);
       const item = document.createElement('li');
       item.innerHTML = `<a class="dropdown-item" href="${match.url}">${match.title}</a>`;
-      resultsContainer.appendChild(item);
+      navbar_resultsContainer.appendChild(item);
     });
-    resultsContainer.classList.add('show');
+    navbar_resultsContainer.classList.add('show');
   } else {
-    resultsContainer.innerHTML = '<li class="dropdown-item disabled">No results found.</li>';
-    resultsContainer.classList.add('show');
+    navbar_resultsContainer.innerHTML = '<li class="dropdown-item disabled">No results found.</li>';
+    navbar_resultsContainer.classList.add('show');
   }
-  document.addEventListener('click', function (event) {
-  const searchForm = document.getElementById('site_search');
-  const resultsContainer = document.getElementById('search_results');
 
-  if (!searchForm.contains(event.target)) {
-    resultsContainer.classList.remove('show');
-  }
-});
+  
+  document.addEventListener('click', function (event) {
+    navbar_resultsContainer.classList.remove('show');
+  });
+  navbar_input.addEventListener('keydown', function(e) {
+    if (e.key === 'Enter') {
+      
+      e.preventDefault();
+      var q = navbar_input.value.trim();
+      
+      if (q.length > 0) {
+         window.location.href = `/search/?${encodeURIComponent(q)}`;
+        
+      }
+    }
+  });
 });
 
